4712ce577daf8677297bd869a3d7a702b7924fbb
Jake Morrison <jake@cogini.com>
2026-07-29 16:39:03 -0500

    Require Elixir 1.15

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule S3Uploader.MixProject do
     [
       app: :s3_uploader,
       version: @version,
-      elixir: "~> 1.19",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
